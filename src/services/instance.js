c0c7f1d4d0120e4fd45295b325ce015ac6aa6758
IndiraRamalingam <indiraramalingam93@gmail.com>
2024-02-08 11:59:02 +0530

    Code commit

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const baseURL='https://e-commerce-dcpc.onrender.com/api'
-const baseURL = 'http://localhost:3001/api'
+const baseURL='https://sky-notes-be.onrender.com/api'
+//const baseURL = 'http://localhost:3001/api'
 
 const authInstance = axios.create({
     baseURL: baseURL,
